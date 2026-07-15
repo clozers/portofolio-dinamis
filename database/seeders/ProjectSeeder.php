d1504d094df2d8f2d0f07e537c03e29e338e2c76
Chairul Imam <111479228+clozers@users.noreply.github.com>
2026-07-15 15:54:50 +0700

    Update ProjectSeeder to use 'tgl_upload' field
    Replaced 'created_at' field with 'tgl_upload' in ProjectSeeder.

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -14,7 +14,7 @@ class ProjectSeeder extends Seeder
             'description' => 'A mobile app to simplify plant care with reminders and categorization.',
             'screenshot' => 'uploads/screenshots/plantify.png',
             'github_link' => 'https://github.com/username/plantify',
-            'created_at' => '2025-05-01',
+            'tgl_upload' => '2025-05-01',
         ]);
 
         Project::create([
@@ -22,7 +22,7 @@ class ProjectSeeder extends Seeder
             'description' => 'A project management tool with intuitive kanban boards.',
             'screenshot' => 'uploads/screenshots/taskmaster.png',
             'github_link' => 'https://github.com/username/taskmaster',
-            'created_at' => '2025-06-01',
+            'tgl_upload' => '2025-06-01',
         ]);
     }
 }
